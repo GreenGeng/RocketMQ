4d5743cd558d0fc62f0cf8a2c0a04782dd62e56c
gengyuan <2088034881@qq.com>
2023-02-18 20:59:40 +0800

    update

---
+++ src/main/java/com/learnbyheart/rocketmqtest1/RocketMqTest1Application.java
@@ -0,0 +1,13 @@
+package com.learnbyheart.rocketmqtest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketMqTest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketMqTest1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/learnbyheart/rocketmqtest1/RocketMqTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnbyheart.rocketmqtest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketMqTest1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
